7180bc6c361f38131c627abbcd6b7f139065d3a1
triom <triomphantesonwa@gmail.com>
2021-11-03 01:51:03 +0100

    second commit

--- generer_graphe_barabasi.py
+++ generer_graphe_barabasi.py
@@ -12,10 +12,28 @@ def graphe_barabasi():
     for i in range(len(G)):
         noeuds.append(i)
     print(noeuds)
+    nouvelleArrete = []
     while m > 0:
+        valeur = random.choice(noeuds)
+        nouvelleArrete.append(valeur)
         # empecher que le random ne prenne la mm valeur deux fois
-        nouveauSommet = random.choice(noeuds)
-        noeuds.remove(nouveauSommet)  #empecher que le random ne prenne la mm valeur deux fois
+        noeuds.remove(valeur)
         m -= 1
-        print(nouveauSommet)
+    print(nouvelleArrete)
+
     # ajouter le nouveau graphe dans la liste de ses voisins
+    nouveauNoeud = len(G)
+    noeudAdj = {nouveauNoeud: nouvelleArrete}
+    print(G)
+    print("before append")
+    print(noeudAdj)
+    G.update(noeudAdj)
+    print(G)
+    print("after update with new adjacency list for new vertex")
+    for k in range(len(nouvelleArrete)):
+        G[nouvelleArrete[k]].append(nouveauNoeud)
+    print(G)
+    print("after update with new adjacency list for old vertices")
+
+
+graphe_barabasi()
--- graphe_alea.py
+++ graphe_alea.py
@@ -2,20 +2,19 @@ import random
 
 
 def generer_graphe():
-    nombreSommets = int(input("Donnez un nombre de sommets\t"))
+    #nombreSommets = int(input("Donnez un nombre de sommets\t"))
+    nombreSommets = 3
     graphe_aleatoire = [
         [0 for i in range(nombreSommets)]for j in range(nombreSommets)]
-    print(str(graphe_aleatoire))
-    # for i in range(0, nombreSommets):
-    #     for j in range(0, nombreSommets):
-    #         valeurProbilite = random.random()
-    #         if (i != j) and valeurProbilite > 0.2:
-    #             print(valeurProbilite)
-    #             graphe_aleatoire = [[i]]
-    #             # print(i)
-    #             # print(j)
-    #         else:
-    #             print("valeur inferieur")
-    #             print(valeurProbilite)
+    for i in range(0, nombreSommets):
+        for j in range(0, nombreSommets):
+            valeurProbilite = random.random()
+            if (i != j) and valeurProbilite > 0.2:
+                graphe_aleatoire[i][j] = 1
+            else:
+                break
+    for i in range(len(graphe_aleatoire)):
+        print(graphe_aleatoire[i])
 
     # reste a stocker les graphes et afficher soit la liste d'adjacence ou la matrice
+generer_graphe()
